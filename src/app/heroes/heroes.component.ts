e8410a6846ad964fbab1ca931e2f0ce3053fdfdf
Norelli-Christian <christian.norelli@gmail.com>
2020-03-30 11:33:27 +0200

    Fine lezione 30/03

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,7 +21,8 @@ constructor(
   private heroService: HeroService)
    {}
    getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
   }
   ngOnInit() {
     this.getHeroes();
